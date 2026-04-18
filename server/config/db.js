8389af0d7eb4d61c3008e24d4745b1c822a50bbf
Abhishek Parihar <abhinan03@gmail.com>
2026-04-18 18:46:30 +0530

    fix: sql

--- server/config/db.js
+++ server/config/db.js
@@ -1,25 +1,5 @@
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_NAME:", process.env.DB_NAME);
-
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  connectTimeout: 10000,
-
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = pool.promise();
