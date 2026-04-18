b31979109dc3241ece7809caf3e3a2764beca3f7
Abhishek Parihar <abhinan03@gmail.com>
2026-04-18 16:53:00 +0530

    debug env

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 console.log("DB_HOST:", process.env.DB_HOST);
 console.log("DB_PORT:", process.env.DB_PORT);
 console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
 
 const mysql = require('mysql2');
 
