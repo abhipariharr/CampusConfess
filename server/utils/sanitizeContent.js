d69bba5072c1dd065eb188969057fa55ed53b941
Abhishek Parihar <abhinan03@gmail.com>
2026-05-16 11:25:11 +0530

    changed placeholder for hidden link text

--- server/utils/sanitizeContent.js
+++ server/utils/sanitizeContent.js
@@ -4,25 +4,25 @@ function sanitizeContent(text) {
   // 📧 Emails
   text = text.replace(
     /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/gi,
-    '[Sharig emails are not allowed. let it be anonymus please]'
+    '[Sharig emails are not allowed. let it be Anonymous please]'
   );
 
   // 📱 Phone numbers
   text = text.replace(
     /(?:\+91[- ]?)?[6-9]\d{9}\b/g,
-    '[Sharig number are not allowed. let it be anonymus please]'
+    '[Sharig number are not allowed. let it be Anonymous please]'
   );
 
   // 🔗 Links
   text = text.replace(
     /(https?:\/\/[^\s]+)|(https\/\/[^\s]+)|(www\.[^\s]+)/gi,
-    '[Sharig Links are not allowed. let it be anonymus please]'
+    '[Sharig Links are not allowed. let it be Anonymous please]'
   );
 
 // 🔗 Hide common links/domains
 text = text.replace(
   /\b\S+\.(com|in|net|org|gg|me|io)\b/gi,
-  '[hidden link]'
+  '[Sharig Links are not allowed. let it be Anonymous please]'
 );
 
   return text;
