a80a84ad4e24bb2c2a9da672c3b8b00d59389f1f
Abhishek Parihar <abhinan03@gmail.com>
2026-05-16 10:56:46 +0530

    restricted: sharing email and link in confession

--- server/routes/confessions.routes.js
+++ server/routes/confessions.routes.js
@@ -1,3 +1,4 @@
+const sanitizeContent = require('../utils/sanitizeContent');
 const express  = require('express');
 const router   = express.Router();
 const { body, validationResult } = require('express-validator');
